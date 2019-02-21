2d7a66736f10548826d7e193a4fa4ce625c1e0bf
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-21 19:52:32 +0100

    Create bot-1.js

---
+++ bot-1.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : Relax`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Relax' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` `,"http://twitch.tv/M3a4x")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
