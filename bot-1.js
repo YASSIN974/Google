a7ac233922fd70622e75e2508c9313e1929067fc
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-21 20:02:10 +0100

    Update bot-1.js

--- bot-1.js
+++ bot-1.js
@@ -12,6 +12,28 @@ client.user.setGame(` `,"http://twitch.tv/M3a4x")
 client.user.setStatus("dnd")
 });
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "332713449215754242") return;
+
+if (message.content.startsWith(prefix + 'setstream')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}`)
+}
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+}
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+});
 
 
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
