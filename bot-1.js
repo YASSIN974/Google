9d260e0b62ac8fc76abaf0b6cd87c5fb6fc9d331
hey <44032576+YASSIN974@users.noreply.github.com>
2019-02-21 20:03:34 +0100

    Update bot-1.js

--- bot-1.js
+++ bot-1.js
@@ -16,7 +16,7 @@ client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
   var argresult = args.join(' ');
-  if (message.author.id !== "332713449215754242") return;
+  if (message.author.id !== "332713449215754242'548207173176524800") return;
 
 if (message.content.startsWith(prefix + 'setstream')) {
   client.user.setGame(argresult, "https://www.twitch.tv/darkknite55");
